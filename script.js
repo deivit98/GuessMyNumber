5df9a644d3420e2570d4bf8edaf06d434c679296
Deivit Cholakov <azazel7298@gmail.com>
2022-03-21 23:39:13 +0200

    Practicing DOM

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+/*
+console.log(document.querySelector(".message").textContent);
+
+
+document.querySelector(".number").textContent =13 ;
+document.querySelector(".score").textContent = 7;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);*/
+let num = Math.trunc(Math.random()*20)+1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    num = Math.trunc(Math.random()*20)+1;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+});
+
+document.querySelector('.check').addEventListener('click', function(){
+   const guess = Number(document.querySelector('.guess').value);
+
+   if (!guess){
+
+       displayMessage('No Number!😒'); 
+
+     }else if(guess === num){
+         displayMessage('Correct Number!🥳')
+         document.querySelector('.number').textContent = num;
+         document.querySelector('body').style.backgroundColor = '#60b347';
+         document.querySelector('.number').style.width = '30rem';
+         if(score > highScore){
+             highScore = score;
+             document.querySelector('.highscore').textContent = highScore;
+         }
+     }else if (guess !== num){
+        if(score > 1){
+            displayMessage(guess > num?'Too High!🤦‍♂️' : 'Too Low!🤦‍♂️');
+            score--;
+            document.querySelector('.score').textContent = score;
+         }else{
+             displayMessage('you lost the game!');
+             document.querySelector('.score').textContent = 0;
+         }
+
+        }
+});
+
+
+
+
