a5172de1a746ca3e70968968d408826195546d58
Deyvit Cholakov <87949806+deivit98@users.noreply.github.com>
2022-03-23 23:29:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 'use strict';
-/*
-console.log(document.querySelector(".message").textContent);
 
-
-document.querySelector(".number").textContent =13 ;
-document.querySelector(".score").textContent = 7;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);*/
 let num = Math.trunc(Math.random()*20)+1;
 let score = 20;
 let highScore = 0;
